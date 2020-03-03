0904b33714643b058d4cba500c536b3d22397610
groverjatin17 <30456535+groverjatin17@users.noreply.github.com>
2020-03-03 16:16:11 +0530

    Adding a new potter reducer in combined file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import reducers_info from './reducers_info.js'
+import potterReducer from './potterReducer.js'
 
 const rootReducer = combineReducers({
-  reducerInfo : reducers_info
+  reducerInfo : reducers_info,
+  potterReducer: potterReducer
 })
 
-export default rootReducer
+export default rootReducer;
