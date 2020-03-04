202c0b1715a5cb121c45af6885f697ad3403a1b8
groverjatin17 <30456535+groverjatin17@users.noreply.github.com>
2020-03-04 16:54:58 +0530

    Constants for potter page

---
+++ src/components/common/global-constants/PotterPageConstants.js
@@ -0,0 +1,17 @@
+const TABLE_HEADER = {
+    HASH:'#',
+    NAME:'NAME',
+    HOUSE:'HOUSE',
+    BLOOD:'BLOOD',
+    WAND:'WAND',
+    BOGGART:'BOGGART',
+    PATRONUS:'PATRONUS',
+    ARMY:'ARMY'
+};
+
+const BLOOD_TYPES = {
+    PURE_BLOOD:'pure-blood',
+    HALF_BLOOD:'half-blood',
+    MUGGLE: 'muggle',
+};
+export {TABLE_HEADER, BLOOD_TYPES};
