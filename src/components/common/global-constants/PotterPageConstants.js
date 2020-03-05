74cf110dc22dbebe53f8021f0313197607dc5081
groverjatin17 <30456535+groverjatin17@users.noreply.github.com>
2020-03-05 20:48:43 +0530

    Added new constants

--- src/components/common/global-constants/PotterPageConstants.js
+++ src/components/common/global-constants/PotterPageConstants.js
@@ -1,3 +1,16 @@
+import HufflePuff from '../../../assets/hufflepuff.png';
+import Slytherin from '../../../assets/slytherin.png';
+import Gryffindor from '../../../assets/gryffindor.png';
+import Ravenclaw from '../../../assets/ravenclaw.png';
+
+import Wand from '../../../assets/Wand.png';
+import Patronus from '../../../assets/Patronus.png';
+import Animagus from '../../../assets/Animagus.png';
+import Boggart from '../../../assets/Boggart.png';
+import OrderOfThePhoenix from '../../../assets/OrderOfThePhoenix.png';
+import BloodType from '../../../assets/BloodType.png';
+
+
 const TABLE_HEADER = {
     HASH:'#',
     NAME:'NAME',
@@ -14,4 +27,30 @@ const BLOOD_TYPES = {
     HALF_BLOOD:'half-blood',
     MUGGLE: 'muggle',
 };
-export {TABLE_HEADER, BLOOD_TYPES};
+
+const HOUSE_IMAGES = {
+    HUFFLEPUFF: HufflePuff,
+    SLYTHERIN: Slytherin,
+    GRYFFINDOR: Gryffindor,
+    REVENCLAW: Ravenclaw
+};
+
+const HOUSE_NAMES = {
+    HUFFLEPUFF: 'hufflePuff',
+    SLYTHERIN: 'slytherin',
+    GRYFFINDOR: 'gryffindor',
+    RAVENCLAW: 'ravenclaw'
+};
+
+const CHARACTER_IMAGES = {
+    WAND: Wand,
+    PATRONUS: Patronus,
+    ANIMAGUS: Animagus,
+    BOGGART: Boggart,
+    ORDER_OF_THE_PHOENIX: OrderOfThePhoenix,
+    BLOOD_TYPE: BloodType,
+
+};
+
+
+export {TABLE_HEADER, BLOOD_TYPES, HOUSE_IMAGES, HOUSE_NAMES, CHARACTER_IMAGES};
