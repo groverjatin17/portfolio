9ff3a0f201b18aeacd80373f72eea7492c71cc93
groverjatin17 <30456535+groverjatin17@users.noreply.github.com>
2020-03-05 20:53:17 +0530

    Added star rating type from semantic instead

--- src/components/TechStackCards.js
+++ src/components/TechStackCards.js
@@ -1,20 +1,13 @@
 import React from 'react'
-import {Card, CardImage, RatingContainer, GreenCell, WhiteCell, Title} from '../styles/Card.style';
+import {Card, CardImage, Title} from '../styles/Card.style';
+import {Rating} from 'semantic-ui-react';
 
 const CardMaker =({rating, image, title}) =>{
 
-    const generateRating = (value) => {
-        let greenArray =new Array(value).fill(0).map((_,key)=> <GreenCell key={key}/>)
-        let whiteArray = new Array(5-value).fill(0).map((_,key)=> <WhiteCell key={key+4}/>)
-        let finalRating= greenArray.concat(whiteArray)
-        return finalRating;
-    }
 
     return(    
         <Card>
-            <RatingContainer>
-                {generateRating(rating)}
-            </RatingContainer>
+            <Rating defaultRating={3} maxRating={5} icon='star' style={{float: 'right', margin: '5% 0% 0% 62%'}} disabled />
             <CardImage src={image} alt={'Loading...'}/>
             <Title >{title} </Title>
         </Card>
