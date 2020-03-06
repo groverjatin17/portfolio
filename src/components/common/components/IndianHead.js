639a5e72b635af7a512df500164d8c25feba75d3
groverjatin17 <30456535+groverjatin17@users.noreply.github.com>
2020-03-06 22:10:31 +0530

    animation files

---
+++ src/components/common/components/IndianHead.js
@@ -0,0 +1,25 @@
+
+import React from 'react'
+import Lottie from 'react-lottie';
+import * as animationData from '../../../styles/animations/9417-indian-hat-animation.json';
+
+export default function IndianHead(props) {
+
+    const defaultOptions = {
+        loop: true, 
+        autoplay: true, 
+        animationData: animationData.default,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+          }
+    }
+    return (
+        <Lottie 
+            options={defaultOptions}
+            height={props.height}
+            width={props.width}
+        />
+    )
+}
+
+
