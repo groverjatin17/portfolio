eae900eb98469db637c660bc35c1e9dc671b1e06
groverjatin17 <30456535+groverjatin17@users.noreply.github.com>
2020-03-09 20:05:24 +0530

    social media footer complete

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,50 +1,40 @@
 import React, { Component } from "react";
-import { FaFacebookSquare } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaYoutube } from "react-icons/fa";
+import { TiSocialFacebook } from "react-icons/ti";
+import { TiSocialGithub } from "react-icons/ti";
+import { TiSocialLinkedin } from "react-icons/ti";
+import { TiSocialTwitter } from "react-icons/ti";
 
+import TipJar from '../assets/tip-jar.png'
 import IndianHead from './common/components/IndianHead';
-import MoneyWallet from './common/components/MoneyWallet';
 
-const marginRight = {
-  marginRight: "6px"
-};
+import '../styles/scss/index.scss';
 
 export default class SocialMedia extends Component {
   render() {
     return (
-      <div
-        style={{ height: "300px", width: "100%", backgroundColor: "#484848" }}
-      >
-        {/* <h2>
-          <span
-            style={{ borderBottom: "2px solid", borderBottomColor: "red" }}
-          >
-            Follow me on Social Media
-          </span>
-        </h2> */}
-        <div style={{margin: '50px 15px 50px 50px', float: 'left'}}>
-        <IndianHead height={'200px'} width={'200px'} />
-        </div>
-        <div style={{borderLeft: '2px solid #929292', height:'80%', display: 'inline-block', verticalAlign: 'top', borderRadius: '50%', marginTop: '33px'}}></div>
-        <div 
-        style={{width: '300px', display: 'inline-block', verticalAlign: 'top', margin: '130px 0 0 20px', color: '#939598'}}>
-          Hi, this website was made with love. I hope you liked it. This portfolio is amalgamation of variety of wonderful libraries
+      <div className='footer' >
+        <div className='footer-column'>
+          <div className='indian-head'>
+            <IndianHead height={'200px'} width={'200px'} />
           </div>
-        <div style={{margin: '50px 15px 50px 30px', display: 'inline-block'}}>
-        <MoneyWallet height={'200px'} width={'200px'} />
-        </div>
-        <div style={{borderLeft: '2px solid #929292', height:'80%', display: 'inline-block', verticalAlign: 'top', borderRadius: '50%', marginTop: '33px'}}></div>
-        <div 
-        style={{width: '300px', display: 'inline-block', verticalAlign: 'top', margin: '130px 0 0 20px', color: '#939598'}}>
-          It took hours and hours to build this website. Wanna donate few extra meals?
+          <div className='divider'></div>
+          <div className='content'> Get in touch with the creator <br />
+              <TiSocialFacebook className='social-media-icon' />
+              <TiSocialGithub className='social-media-icon'/>
+              <TiSocialLinkedin className='social-media-icon'/>
+              <TiSocialTwitter className='social-media-icon'/>
           </div>
-        
-        {/* <FaFacebookSquare size="2em" style={marginRight} />
-        <FaTwitter size="2em" style={marginRight} />
-        <FaInstagram size="2em" style={marginRight} />
-        <FaYoutube size="2em" style={marginRight} /> */}
+        </div>
+        <div className= 'footer-column'>
+          <img src={TipJar} alt='tipjar' className='tipjar' />
+          <div className='divider'></div>
+            <div className='content'> It took hours and hours to build this website. Wanna donate few extra meals? 
+            <button className='' onClick={() => {console.log('do  this');}} >Donate</button>
+          </div>
+        </div>
+        <div className='termsFooter' >
+          <p>| <a href='#privacy' > Privacy Policy</a> | <a href='#terms'>Terms of Service</a>|</p>
+        </div>
       </div>
     );
   }
