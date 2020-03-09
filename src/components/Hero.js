90a22692978014ef6ac3f33bc969c6ee0bf0b7e3
groverjatin17 <30456535+groverjatin17@users.noreply.github.com>
2020-03-09 20:02:59 +0530

    Fixed button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,42 +1,33 @@
 import React, { Component } from "react";
 import giphy from "../assets/city.gif";
-// import "../styles/scss/cursiveFont.scss"
 import "../styles/scss/index.scss"
-import {Container, ImageOverlay, Giphy, ImageCenterDiv, PortfolioLogo, WelcomeMessage} from '../styles/Hero.style'
+import {Container, ImageOverlay, Giphy, Button, ImageCenterDiv, WelcomeMessage} from '../styles/Hero.style'
 
+import NavigationBar from './NavigationBar';
+
+import '../styles/scss/index.scss';
 
 export default class Hero extends Component {
   render() {
     return (
       <>
-        <Container>
+        <Container
+            className='top-panel'
+            >
           <ImageOverlay></ImageOverlay>
-          {/* <PortfolioLogo>Jatin Grover</PortfolioLogo> */}
-
-
-          <p className='logo'>Jatin Grover</p>
+          <NavigationBar />
           <Giphy
             src={giphy}
             alt={"Loading..."}
           />
-
           <ImageCenterDiv>
             <WelcomeMessage>
               Hi, I am Jatin. I am a Web Developer.
               <br/>
               I build interactive Websites
             </WelcomeMessage>
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center"
-              }}
-            >
-              <input type="button" value="Click Me" />
-            </div>
           </ImageCenterDiv>
-          {/* <div style={scrollDown}>Click here to go down</div> */}
+          <Button onClick={() => {console.log('My Projects');}}>View Projects</Button>
         </Container>
       </>
     );
